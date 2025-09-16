20661ddaf3dfb2ab9d0da8c58b613260ecf10a35
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2025-09-16 17:51:27 +0530

    java varibles

---
+++ 01_Courses/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld{
+
+    public static void main (String args[]){
+    
+        int num1=3;
+        int num2=5;
+        int result =num1+num2;
+    
+        System.out.println("The result of above operation =>"+ result);
+        System.out.println("Hello World ");
+    }
+}
