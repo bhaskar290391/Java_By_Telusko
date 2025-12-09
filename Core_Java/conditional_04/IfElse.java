0b30d0288c23f4bffed35d60d30bf6ea690f1139
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2025-12-09 18:03:19 +0530

    condtional statement and coding exercise

---
+++ Core_Java/conditional_04/IfElse.java
@@ -0,0 +1,13 @@
+
+public class IfElse {
+    public static void main(String[] args) {
+
+        int x = 18;
+
+        if ((x > 10) && (x < 20)) {
+            System.out.println("Hello");
+        } else {
+            System.out.println("Bye");
+        }
+    }
+}
---
+++ Core_Java/conditional_04/Ternary.java
@@ -0,0 +1,8 @@
+class Ternary {
+    public static void main(String[] args) {
+        int number = 5;
+        int result = (number % 2 == 0) ? 10 : 20;
+
+        System.out.println(result);
+    }
+}
